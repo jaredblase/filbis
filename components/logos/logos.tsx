46a99064bef6ad17502c6c5ae3fee5460fab475a
Jared Sy <jaredblase@gmail.com>
2023-10-13 17:33:52 +0800

    update: make logos responsive

--- components/logos/logos.tsx
+++ components/logos/logos.tsx
@@ -3,9 +3,9 @@ import dlsuLogo from '@/public/assets/dlsu-logo.svg'
 
 export function Logos() {
 	return (
-		<div className="flex gap-x-6">
-			<Image src={dlsuLogo} alt="DLSU Logo" className="w-28" priority />
-			<p className="container-block flex h-28 w-auto items-center font-shrikhand text-secondary-100">
+		<div className="flex gap-x-2 md:gap-x-6">
+			<Image src={dlsuLogo} alt="DLSU Logo" className="w-14 md:w-28" priority />
+			<p className="container-block flex h-14 md:h-28 items-center font-shrikhand text-secondary-100">
 				<span className="text-[46cqh]">Fil</span>
 				<span className="ml-2 block aspect-square rounded-full bg-secondary-100 p-[6cqh]">
 					<span className="inline-flex aspect-square items-center rounded-full bg-primary-400 p-[6cqh] text-[46cqh]">
