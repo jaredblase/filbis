902be2a273691601a5b7d8b2ae699433247e8d0f
cifelse <meetcif@gmail.com>
2023-10-18 09:31:18 +0800

    Changed brand color from blue to green

--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,16 @@ export default async function HomePage() {
 					preserveAspectRatio="none"
 					viewBox="0 0 1280 832"
 					className="h-full w-full"
-				>
-					<path d="M0 0H1280V59L0 384V0Z" fill="#215386" />
-					<path d="M0 832H392L0 384V832Z" fill="#306CA8" />
+				>	
+					<path d="M0 0H1280V59C1280 59 570 464 0 384C0 234.039 0 0 0 0Z" fill="#2C6839"/>
+					<path d="M0 832H675.5C399.73 774.766 30.365 676.747 0 384C0 558.955 0 832 0 832Z" fill="#408B51"/>
 				</svg>
 			</div>
 			<HomeMenu />
 			<section className="text-secondary-100">
-				<div className="mx-auto mb-6 w-fit rounded-3xl bg-primary-400 px-10 py-5">
+				{/* <div className="mx-auto mb-6 w-fit rounded-3xl bg-primary-400 px-10 py-5">
 					<p className="text-center font-shrikhand text-3xl">Welcome back!</p>
-				</div>
+				</div> */}
 				<h1 className="h1 with-shadow text-center">So, how are you?</h1>
 				<FilbisAvatar className="center m-auto w-[75%] max-w-lg" />
 			</section>
