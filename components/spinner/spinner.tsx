0f7f79c27d4551a223aef8a87434f678325769a2
Jared Sy <jaredblase@gmail.com>
2023-11-07 16:12:55 +0800

    add: spinner component

---
+++ components/spinner/spinner.tsx
@@ -0,0 +1,13 @@
+import { HTMLAttributes } from 'react'
+import styles from './spinner.module.css'
+
+export function Spinner({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
+	return (
+		<div className={styles['lds-ellipsis'] + (className ? ' ' + className : '')} {...props}>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	)
+}
