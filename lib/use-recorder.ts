4325a7c8c57b7770f2f4cbbae75edd17deb063fe
Jared Sy <jaredblase@gmail.com>
2023-11-08 17:58:42 +0800

    update: use record hoook

--- lib/use-recorder.ts
+++ lib/use-recorder.ts
@@ -14,14 +14,14 @@ export function useRecorder() {
 		const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
 		const recorder = new MediaRecorder(stream)
 
-		recorder.ondataavailable = e => chunks.current.push(e.data)
+		recorder.ondataavailable = e => {
+			chunks.current.push(e.data)
+		}
 
 		return recorder
 	}
 
 	async function start() {
-		chunks.current = []
-
 		if (!recorder.current) {
 			recorder.current = await createRecorder()
 		}
@@ -33,13 +33,29 @@ export function useRecorder() {
 	function stop() {
 		recorder.current?.stop()
 		setIsRecording(false)
+
+		// resolve when recorder has finished processing data
+		return new Promise<void>(resolve => {
+			const int = setInterval(() => {
+				if (chunks.current.length) {
+					resolve()
+					clearInterval(int)
+				}
+			}, 200)
+		})
+	}
+
+	function clearData() {
+		chunks.current = []
 	}
 
 	return {
 		start,
 		stop,
-		get data() {
-			return chunks.current
+		clearData,
+		getFile() {
+			if (!chunks.current.length) return
+			return new File(chunks.current, 'audio.wav', { type: 'audio/wav' })
 		},
 		isRecording,
 	}
