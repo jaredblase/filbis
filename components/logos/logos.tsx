9623bfc0a00ae9bd83089e9f89c7d0fca848f407
Mauries Lopez <mauilopez00@gmail.com>
2024-06-28 23:42:01 +0800

    Major UI Update
    - Successfully made the website compatible with all different resolutions and fixed all the sizes issues. All are now responsive

--- components/logos/logos.tsx
+++ components/logos/logos.tsx
@@ -5,8 +5,8 @@ import dostSeal from '@/public/assets/dost-seal.svg'
 export function Logos() {
 	return (
 		<div className="relative flex gap-x-2 md:gap-x-6 md:items-center sm:items-center xs:items-center">
-			<Image src={dlsuLogo} alt="DLSU Logo" className="sm:w-[80px] xs:w-[20px]" priority />
-			<Image src={dostSeal} alt="DOST Seal" className="sm:w-[80px] xs:w-[20px]" />
+			<Image src={dlsuLogo} alt="DLSU Logo" className="sm:w-[60px] xs:w-[50px]" priority />
+			<Image src={dostSeal} alt="DOST Seal" className="sm:w-[60px] xs:w-[50px]" />
 			<p className="container-block flex sm:h-[80px] xs:h-[60px] items-center font-shrikhand text-secondary-100">
 				<span className="text-[46cqh]">Fil</span>
 				<span className="ml-2 block aspect-square rounded-full bg-secondary-100 p-[6cqh]">
