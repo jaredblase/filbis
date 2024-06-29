f4e4b7a28dd7bb7afd777e3fc1e465b6c14f7184
Mauries Lopez <mauilopez00@gmail.com>
2024-06-29 19:52:51 +0800

    Quick fix
    - Choices are now alligned.
    - New small detail in the UI

--- components/logos/logos.tsx
+++ components/logos/logos.tsx
@@ -5,9 +5,9 @@ import dostSeal from '@/public/assets/dost-seal.svg'
 export function Logos() {
 	return (
 		<div className="relative flex gap-x-2 md:gap-x-6 md:items-center sm:items-center xs:items-center">
-			<Image src={dlsuLogo} alt="DLSU Logo" className="sm:w-[60px] xs:w-[50px]" priority />
-			<Image src={dostSeal} alt="DOST Seal" className="sm:w-[60px] xs:w-[50px]" />
-			<p className="container-block flex sm:h-[80px] xs:h-[60px] items-center font-shrikhand text-secondary-100">
+			<Image src={dlsuLogo} alt="DLSU Logo" className="sm:w-[60px] xs:w-[40px]" priority />
+			<Image src={dostSeal} alt="DOST Seal" className="sm:w-[60px] xs:w-[40px]" />
+			<p className="container-block flex sm:h-[80px] xs:h-[40px] items-center font-shrikhand text-secondary-100">
 				<span className="text-[46cqh]">Fil</span>
 				<span className="ml-2 block aspect-square rounded-full bg-secondary-100 p-[6cqh]">
 					<span className="inline-flex aspect-square items-center rounded-full bg-[#ED7042] p-[6cqh] text-[46cqh]">
